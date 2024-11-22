fcf9680b75ceaace2391d0c06ea1b6ed9a898201
79930 <likrakstrimy@gmail.ru>
2024-11-22 15:42:11 +0700

    Добавлена функция сохранения в формате json

--- fileIO_main.py
+++ fileIO_main.py
@@ -4,6 +4,20 @@ from tkinter import ttk
 import requests
 from tkinter import messagebox as mb
 import pyperclip
+import json
+import os
+
+history_file = 'upload_history.json'
+
+
+def save_history(file_path, link):
+    history = []
+    if os.path.exists(history_file):
+        with open(history_file, 'r') as f:
+            history = json.load(f)
+    history.append({'file_path': os.path.basename(file_path), 'download_link': link})
+    with open(history_file, 'w') as f:
+        json.dump(history, f, indent=4)
 
 
 def upload():
@@ -18,6 +32,7 @@ def upload():
                 entry.delete(0, END)
                 entry.insert(0, link)
                 pyperclip.copy(link)
+                save_history(filepath, link)
                 mb.showinfo('Ссылка скопирована', f'Ссылка {link} скопирована')
     except Exception as e:
         mb.showerror('Ошибка!', f'Произошла ошибка {e}')
