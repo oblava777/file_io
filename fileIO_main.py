9438c669090055c959f6dda9a14bdeb5fa8f3266
79930 <likrakstrimy@gmail.ru>
2024-11-22 15:28:23 +0700

    Добавлено автоматическое копирование в буфер обмена

--- fileIO_main.py
+++ fileIO_main.py
@@ -3,6 +3,7 @@ from tkinter import filedialog as fd
 from tkinter import ttk
 import requests
 from tkinter import messagebox as mb
+import pyperclip
 
 
 def upload():
@@ -16,6 +17,8 @@ def upload():
                 link = response.json()['link']
                 entry.delete(0, END)
                 entry.insert(0, link)
+                pyperclip.copy(link)
+                mb.showinfo('Ссылка скопирована', f'Ссылка {link} скопирована')
     except Exception as e:
         mb.showerror('Ошибка!', f'Произошла ошибка {e}')
 
